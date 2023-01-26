edad22e9e2e83a51d9d2fb0ca6cdff12c50d674a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-01-26 07:11:14 +0100

    Basic users endpoint get requests

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,6 @@
 const db = require("../db/queries");
 
-const allUsers = async (req, res) => {
+const getUsers = async (req, res) => {
     try {
         const result = await db.query("SELECT * FROM users ORDER BY userid ASC");
         return res.status(200).json(result);
@@ -11,4 +11,16 @@ const allUsers = async (req, res) => {
     }
 }
 
-module.exports = { allUsers };
+const getUserById = async (req, res) => {
+    const requestedId = parseInt(req.params.id);
+    try {
+        const result = await db.query("SELECT * FROM users WHERE userid = $1", [requestedId]);
+        return res.status(200).json(result[0]);
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).send();
+    }
+}
+
+module.exports = { getUsers, getUserById };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController.js")
 
-router.get("/", usersController.allUsers);
+router.get("/", usersController.getUsers);
+router.get("/:id", usersController.getUserById);
 
 module.exports = router;
