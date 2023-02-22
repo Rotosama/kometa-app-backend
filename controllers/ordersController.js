f22d32da7984c9dcf1c686419372cbcf6fb9da39
Rotosama <n.ruizgarcia@hotmail.com>
2023-02-22 21:15:40 +0100

    validator

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -2,125 +2,125 @@ const OrdersManager = require("../models/order");
 const assignOrder = require("../services/assignOrder");
 
 const getOrders = async (req, res) => {
-    let result;
-    try {
-        if (req.user.userRole === 'client') {
-            result = await OrdersManager.getAllByClient(req.user.userUUID);
-            return res.status(200).json(result);
-        }
-        if (req.user.userRole === 'delivery') {
-            result = await OrdersManager.getAllByDelivery(req.user.userUUID);
-            return res.status(200).json(result);
-        }
-        result = await OrdersManager.getAll();
-        return res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  let result;
+  try {
+    if (req.user.userRole === "client") {
+      result = await OrdersManager.getAllByClient(req.user.userUUID);
+      return res.status(200).json(result);
     }
+    if (req.user.userRole === "delivery") {
+      result = await OrdersManager.getAllByDelivery(req.user.userUUID);
+      return res.status(200).json(result);
+    }
+    result = await OrdersManager.getAll();
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const getOrderById = async (req, res) => {
-    const requestedId = req.params.id;
-    try {
-        const result = await OrdersManager.getOneById(requestedId);
-        if (result) {
-            return res.status(200).json(result);
-        } else {
-            return res.status(404).send();
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  const requestedId = req.params.id;
+  try {
+    const result = await OrdersManager.getOneById(requestedId);
+    if (result) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(404).send();
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const getOrderByClientID = async (req, res) => {
-    const requestedId = req.params.id;
-    try {
-        const result = await OrdersManager.getAllByClient(requestedId);
-        if (result) {
-            return res.status(200).json(result);
-        }
-        return res.status(404).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  const requestedId = req.params.id;
+  try {
+    const result = await OrdersManager.getAllByClient(requestedId);
+    if (result) {
+      return res.status(200).json(result);
     }
+    return res.status(404).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const getOrderByDeliveryID = async (req, res) => {
-    const requestedId = req.params.id;
-    try {
-        const result = await OrdersManager.getAllByDelivery(requestedId);
-        if (result) {
-            return res.status(200).json(result);
-        }
-        return res.status(404).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  const requestedId = req.params.id;
+  try {
+    const result = await OrdersManager.getAllByDelivery(requestedId);
+    if (result) {
+      return res.status(200).json(result);
     }
+    return res.status(404).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const createOrder = async (req, res) => {
-    if (req.user.userRole !== "client") {
-        return res.status(401).send();
+  if (req.user.userRole !== "client") {
+    return res.status(401).send();
+  }
+  const newOrder = {
+    clientUUID: req.user.userUUID,
+    orderDate: req.body.orderDate,
+    orderStatus: "Pending",
+    orderCharge: req.body.orderCharge,
+    originLatitude: req.body.originLatitude,
+    originLongitude: req.body.originLongitude,
+    destinationLatitude: req.body.destinationLatitude,
+    destinationLongitude: req.body.destinationLongitude,
+    description: req.body.description,
+  };
+  try {
+    const result = await OrdersManager.createOrder(newOrder);
+    if (result) {
+      // call the assigning function
+      assignOrder();
+      return res.status(201).json(result);
     }
-    const newOrder = {
-        clientUUID: req.user.userUUID,
-        orderDate: req.body.orderDate,
-        orderStatus: "Pending",
-        orderCharge: req.body.orderCharge,
-        originLatitude: req.body.originLatitude,
-        originLongitude: req.body.originLongitude,
-        destinationLatitude: req.body.destinationLatitude,
-        destinationLongitude: req.body.destinationLongitude,
-        description: req.body.description,
-    };
-    try {
-        const result = await OrdersManager.createOrder(newOrder);
-        if (result) {
-            // call the assigning function
-            assignOrder();
-            return res.status(201).json(result);
-        }
-        return res.status(400).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
-    }
+    return res.status(400).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const updateOrder = async (req, res) => {
-    const updatedOrder = {
-        orderStatus: req.body.orderStatus,
-        orderUUID: req.params.id,
-    };
-    try {
-        const result = await OrdersManager.updateStatus(updatedOrder);
-        if (result) {
-            return res.status(201).json(result);
-        }
-        return res.status(400).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  const updatedOrder = {
+    orderStatus: req.body.orderStatus,
+    orderUUID: req.params.id,
+  };
+  try {
+    const result = await OrdersManager.updateStatus(updatedOrder);
+    if (result) {
+      return res.status(201).json(result);
     }
+    return res.status(400).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 const deleteOrder = async (req, res) => {
-    const requestedId = req.params.id;
-    try {
-        const result = await OrdersManager.deleteOrder(requestedId);
-        if (result) {
-            return res.status(200).json(result);
-        }
-        return res.status(404).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+  const requestedId = req.params.id;
+  try {
+    const result = await OrdersManager.deleteOrder(requestedId);
+    if (result) {
+      return res.status(200).json(result);
     }
+    return res.status(404).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
 };
 
 module.exports = {
@@ -128,5 +128,5 @@ module.exports = {
   getOrderById,
   createOrder,
   updateOrder,
-  deleteOrder
+  deleteOrder,
 };
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -12,109 +12,109 @@ Route user creation according to role
 */
 
 const register = async (req, res) => {
-    const existingUser = await UsersManager.getUserByEmailOrNationalId(req.body.email, req.body.nationalID);
-    if (existingUser) {
-        return res.status(400).json({ error: "User already exists" });
+  const existingUser = await UsersManager.getUserByEmailOrNationalId(
+    req.body.email,
+    req.body.nationalID
+  );
+  if (existingUser) {
+    return res.status(400).json({ error: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  req.body.password = hashedPassword;
+  try {
+    let newUser;
+    switch (req.body.role) {
+      case "admin":
+        newUser = await registerAdmin(req.body);
+        break;
+      case "client":
+        newUser = await registerClient(req.body);
+        break;
+      case "delivery":
+        newUser = await registerDeliverer(req.body);
+        break;
+      default:
+        return res.status(400).json({ error: "Invalid enrolment" });
     }
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    req.body.password = hashedPassword;
-    try {
-        let newUser;
-        switch (req.body.role) {
-            case "admin":
-                newUser = await registerAdmin(req.body);
-                break;
-            case "client":
-                newUser = await registerClient(req.body);
-                break;
-            case "delivery":
-                newUser = await registerDeliverer(req.body);
-                break;
-            default:
-                return res.status(400).json({ error: "Invalid enrolment" });
-        }
-        if (newUser) {
-            return res.status(201).json({ message: "User created" });
-        }
-        return res.status(400).send();
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send();
+    if (newUser) {
+      return res.status(201).json({ message: "User created" });
     }
-}
+    return res.status(400).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
+};
 
 const registerAdmin = async (userInfo) => {
-    const newAdmin = new Admin(
-        userID = 0,
-        userInfo.firstName,
-        userInfo.lastName,
-        userInfo.birthdate,
-        userInfo.nationalID,
-        userInfo.phone,
-        userInfo.email,
-        userInfo.password,
-        userUUID = 0,
-        adminID = 0
-    );
-    try {
-        const result = await AdminsManager.createAdmin(newAdmin);
-        return (result);
-    }
-    catch (error) {
-        console.error(error);
-        return (null);
-    }
-}
+  const newAdmin = new Admin(
+    (userID = 0),
+    userInfo.firstName,
+    userInfo.lastName,
+    userInfo.birthdate,
+    userInfo.nationalID,
+    userInfo.phone,
+    userInfo.email,
+    userInfo.password,
+    (userUUID = 0),
+    (adminID = 0)
+  );
+  try {
+    const result = await AdminsManager.createAdmin(newAdmin);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 const registerClient = async (userInfo) => {
-    const newClient = new Client(
-        userID = 0,
-        userInfo.firstName,
-        userInfo.lastName,
-        userInfo.birthdate,
-        userInfo.nationalID,
-        userInfo.phone,
-        userInfo.email,
-        userInfo.password,
-        userUUID = 0,
-        clientID = 0,
-        userInfo.defaultLatitude,
-        userInfo.defaultLongitude
-    );
-    try {
-        const result = await ClientsManager.createClient(newClient);
-        return (result);
-    }
-    catch (error) {
-        console.error(error);
-        return res.status(500).send();
-    }
-}
+  const newClient = new Client(
+    (userID = 0),
+    userInfo.firstName,
+    userInfo.lastName,
+    userInfo.birthdate,
+    userInfo.nationalID,
+    userInfo.phone,
+    userInfo.email,
+    userInfo.password,
+    (userUUID = 0),
+    (clientID = 0),
+    userInfo.defaultLatitude,
+    userInfo.defaultLongitude
+  );
+  try {
+    const result = await ClientsManager.createClient(newClient);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send();
+  }
+};
 
 const registerDeliverer = async (userInfo) => {
-    const newDeliverer = new Deliverer(
-        userID = 0,
-        userInfo.firstName,
-        userInfo.lastName,
-        userInfo.birthdate,
-        userInfo.nationalID,
-        userInfo.phone,
-        userInfo.email,
-        userInfo.password,
-        userUUID = 0,
-        deliveryID = 0,
-        userInfo.isAvailable,
-        userInfo.currentLatitude,
-        userInfo.currentLongitude
-    );
-    try {
-        const result = await DeliverersManager.createDeliverer(newDeliverer);
-        return (result);
-    }
-    catch (error) {
-        console.error(error);
-        return (null);
-    }
-}
+  const newDeliverer = new Deliverer(
+    (userID = 0),
+    userInfo.firstName,
+    userInfo.lastName,
+    userInfo.birthdate,
+    userInfo.nationalID,
+    userInfo.phone,
+    userInfo.email,
+    userInfo.password,
+    (userUUID = 0),
+    (deliveryID = 0),
+    userInfo.isAvailable,
+    userInfo.currentLatitude,
+    userInfo.currentLongitude
+  );
+  try {
+    const result = await DeliverersManager.createDeliverer(newDeliverer);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 module.exports = { register };
